ff6b7a1824b2d993815fb48e1096e7c240f278d2
immonroe <immanuelmonroe@gmail.com>
2025-06-19 20:29:03 -0400

    Fix: Made fix to allow for Postman to test routes for /api/items

--- src/main/java/com/drip/backend/controllers/ItemController.java
+++ src/main/java/com/drip/backend/controllers/ItemController.java
@@ -2,25 +2,32 @@ package com.drip.backend.controllers;
 
 import com.drip.backend.models.Item;
 import com.drip.backend.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// marks class as handling HTTP req + returns JSON
 @RestController
+
+// prefix for all endpoints
 @RequestMapping("/api/items")
 public class ItemController {
 
     private final ItemRepository repo;
 
+    @Autowired
     public ItemController(ItemRepository repo) {
         this.repo = repo;
     }
 
+    // GET req
     @GetMapping
-    public List<Item> getAll() {
+    public List<Item> getAllItems() {
         return repo.findAll();
     }
 
+    // POST req
     @PostMapping
     public Item create(@RequestBody Item item) {
         return repo.save(item);
