216a539ddf37dc2e4893acb1d63e2d3769d1f80c
immonroe <immanuelmonroe@gmail.com>
2025-06-26 17:44:38 -0400

    feat: add frontend form to create new items via POST
    - Added AddItemForm component with controlled inputs
    - Implemented POST request to backend API on form submission
    - Integrated form into page.tsx and triggered list refresh after submit
    - Verified fullstack POST flow (React ➝ Spring Boot ➝ PostgreSQL)

--- drip-frontend/src/app/page.tsx
+++ drip-frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
+"use client"
+
+import { useState } from "react"
 import ItemList from "../components/ItemList"
+import AddItemForm from "../components/AddItemForm"
 
 export default function Home() {
+
+  // used to re-render list when new item is added
+  const [refreshKey, setRefreshKey] = useState(0)
+
+  const handleItemAdded = () => {
+    setRefreshKey((prev) => prev + 1)
+  }
   return (
-    <main className="p-8">
+    <main className="p-8 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-6">All Items</h1>
-      <ItemList />
+      <AddItemForm onItemAdded={handleItemAdded} />
+      {/* refresh key below is telling react that when a new item is added to mount ItemList again */}
+      <ItemList key={refreshKey} />
     </main>
   )
 }
